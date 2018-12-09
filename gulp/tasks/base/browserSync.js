d41e6fe44211afeebb8e75ad5ba12af9ac74ff06
Nicholas Adamou <nicholasadamouemail@gmail.com>
2018-12-09 08:23:57 -0600

    fixed an issue with 'browserSync.js'

--- gulp/tasks/base/browserSync.js
+++ gulp/tasks/base/browserSync.js
@@ -27,14 +27,25 @@ gulp.task('browsersync', (done) => {
   gulp.watch(`${paths.to.assets.in}/images/**/*`, gulp.series('images'))
   gulp.watch(`${paths.to.assets.in}/media/**/*`, gulp.series('media'))
   gulp.watch(`${paths.to.assets.in}/misc/**/*`, gulp.series('misc'))
-  gulp.watch(`${paths.to.js.in}/**/*.js`, gulp.series('js'))
-  gulp.watch(`${paths.to.sass.in}/**/*.scss`, gulp.series('sass'))
-  gulp.watch(`${paths.to.vendors}/*.js`, gulp.series('vendors'))
-  // Because we inline the CSS and clean the compiled files
-  // from the build folder, we need to re-compile each file-type
-  // and re-inject into the HTML document on each save then, finally
-  // re-clean the build directory.
-  gulp.watch(`${paths.to.pug.in}/**/*.pug`, gulp.series('sass', 'js', 'vendors', 'pug', 'clean:compiled-files'))
+
+  if (config.environment === 'production') {
+    // During 'production' we inline the CSS and JS so, we clean
+    // the compiled files from the build folder. Because of this,
+    // we need to re-compile each file-type and re-inject into the
+    // HTML document on each save then, finally re-clean the build directory.
+    gulp.watch(`${paths.to.vendors}/*.js`, gulp.series('sass', 'js', 'vendors', 'pug', 'clean:compiled-files'))
+    gulp.watch(`${paths.to.js.in}/**/*.js`, gulp.series('sass', 'js', 'vendors', 'pug', 'clean:compiled-files'))
+    gulp.watch(`${paths.to.sass.in}/**/*.scss`, gulp.series('sass', 'js', 'vendors', 'pug', 'clean:compiled-files'))
+    gulp.watch(`${paths.to.pug.in}/**/*.pug`, gulp.series('sass', 'js', 'vendors', 'pug', 'clean:compiled-files'))
+  } else {
+    // During 'development' we do not inline the CSS and JS,
+    // thus we do not need to re-compile each file-type and
+    // re-inject into the HTML document on each save.
+    gulp.watch(`${paths.to.pug.in}/**/*.pug`, gulp.series('pug'))
+    gulp.watch(`${paths.to.vendors}/*.js`, gulp.series('vendors'))
+    gulp.watch(`${paths.to.js.in}/**/*.js`, gulp.series('js'))
+    gulp.watch(`${paths.to.sass.in}/**/*.scss`, gulp.series('sass'))
+  }
 
   done()
 })
